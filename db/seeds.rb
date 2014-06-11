7eefa99e54a115677eadc9e20f8b8bf80eb54b6e
GabrielCL16 <gabriel_cruz_q606@hotmail.com>
2014-06-11 17:38:58 -0500

    creacion de grupos e,f,g y h
    se crearon los grupos y se asignaron sus partidos

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,83 +16,83 @@ if not camerun
 	camerun = Team.new nombre: 'Camerun', abreviatura: 'cmr'
 end
 
-suziza = Team.find_by abreviatura:'sui'
+suziza = Team.find_by abreviatura: 'sui'
 if not suziza
    suiza = Team.new nombre: 'Suiza', abreviatura: 'sui'
 end
 
-ecuador = Team.find_by abreviatura:'ecu'
+ecuador = Team.find_by abreviatura: 'ecu'
 if not ecuador
 	ecuador = Team.new nombre: 'Ecuador', abreviatura: 'ecu'
 end
 
 francia = Team.find_by abreviatura: 'fra'
 if not francia
-   francia = Team.new nombre: 'Francia' abreviatura: 'fra'
+   francia = Team.new nombre:'Francia', abreviatura:'fra'
 end
 
 honduras = Team.find_by abreviatura: 'hon'
 if not honduras
-	honduras = Team.new nombre: 'Honduras' abreviatura: 'hon'
+	honduras = Team.new nombre: 'Honduras', abreviatura: 'hon'
 end
 
 argentina = Team.find_by abreviatura: 'arg'
 if not argentina
-	argentina = Team.new nombre: 'Argentina' abreviatura: 'arg'
+	argentina = Team.new nombre: 'Argentina', abreviatura: 'arg'
 end
 
 bosnia = Team.find_by abreviatura: 'bih'
 if not bosnia 
-	bosnia = Team.new nombre: 'Bosnia' abreviatura: 'bih'
+	bosnia = Team.new nombre: 'Bosnia', abreviatura: 'bih'
 end
 
 iran = Team.find_by abreviatura: 'irn'
 if not iran
-	iran = Team.new nombre: 'Irán' abreviatura: 'irn'
+	iran = Team.new nombre: 'Irán', abreviatura: 'irn'
 end
 
 nigeria = Team.find_by abreviatura: 'nga'
 if not nigeria
-	nigeria = Team.new nombre: 'Nigeria' abreviatura: 'nga'
+	nigeria = Team.new nombre: 'Nigeria', abreviatura: 'nga'
 end
 
 alemania = Team.find_by abreviatura: 'ger'
 if not alemania
-	alemania = Team.new nombre: 'Alemania' abreviatura: 'ger'
+	alemania = Team.new nombre: 'Alemania', abreviatura: 'ger'
 end
 
 portugal = Team.find_by abreviatura: 'por'
 if not portugal 
-	portugal = Team.new nombre: 'Portugal' abreviatura: 'por'
+	portugal = Team.new nombre: 'Portugal', abreviatura: 'por'
 end
 
 ghana = Team.find_by abreviatura: 'gha'
 if not ghana
-	ghana = Team.new nombre: 'Ghana' abreviatura: 'gha'
+	ghana = Team.new nombre: 'Ghana', abreviatura: 'gha'
 end
 
 estados_unidos = Team.find_by abreviatura: 'usa'
 if not estados_unidos
-	estados_unidos = Team.new nombre: 'Estados Unidos' abreviatura: 'USA'
+	estados_unidos = Team.new nombre: 'Estados Unidos', abreviatura: 'USA'
 
 belgica = Team.find_by abreviatura: 'bel'
 if not belgica 
-	belgica = Team.new nombre: 'Bélgica' abreviatura: 'bel'
+	belgica = Team.new nombre: 'Bélgica', abreviatura: 'bel'
 end
 
 argelia = Team.find_by abreviatura: 'alg'
 if not argelia 
-	argelia = Team.new nombre: 'Argelia' abreviatura: 'alg'
+	argelia = Team.new nombre: 'Argelia', abreviatura: 'alg'
 end
 
 rusia = Team.find_by abreviatura: 'rus'
 if not rusia
-	rusia = Team.new nombre: 'Rusia' abreviatura: 'rus'
+	rusia = Team.new nombre: 'Rusia', abreviatura: 'rus'
 end
 
 republica_de_corea Team.find_by abreviatura:'kor'
 if not republica_de_corea
-	republica_de_corea = Team.new nombre: 'República de corea' abreviatura: 'kor'
+	republica_de_corea = Team.new nombre: 'República de corea', abreviatura: 'kor'
 end
 
 
@@ -173,149 +173,149 @@ if not partido_1
 partido_1 = Game.new fecha: fecha_partido_1, local:mexico, visita:camerun, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
 end
 
-fecha_partido_26 = DateTime.parse("2014-06-15 11:00:00")
-partido_26 = Game.where(fecha: fecha_partido_26, local: suiza, visita: ecuador).first
+fecha_partido_25 = DateTime.parse("2014-06-15 11:00:00")
+partido_25 = Game.where(fecha: fecha_partido_25, local: suiza, visita: ecuador).first
+if not partido_25
+partido_25 = Game.new fecha: fecha_partido_25, local: suiza, visita:ecuador, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
+end
+
+fecha_partido_26 = DateTime.parse("2014-06-15 14:00:00")
+partido_26 = Game.where(fecha: fecha_partido_26, local: francia, visita: honduras).first
 if not partido_26
-partido_26 = Game.new fecha: fecha_partido_26, local: suiza, visita:ecuador, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
+partido_26 = Game.new fecha: fecha_partido_26, local:francia, visita:honduras, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
 end
 
-fecha_partido_27 = DateTime.parse("2014-06-15 14:00:00")
-partido_27 = Game.where(fecha: fecha_partido_27, local: francia, visita: honduras).first
+fecha_partido_27 = DateTime.parse("2014-06-15 17:00:00")
+partido_27 = Game.where(fecha: fecha_partido_27, local: argentina, visita: bosnia).first
 if not partido_27
-partido_27 = Game.new fecha: fecha_partido_27, local:francia, visita:honduras, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
+partido_27 = Game.new fecha: fecha_partido_27, local:argentina, visita:bosnia, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
 end
 
-fecha_partido_28 = DateTime.parse("2014-06-15 17:00:00")
-partido_28 = Game.where(fecha: fecha_partido_28, local: argentina, visita: bosnia).first
+fecha_partido_28 = DateTime.parse("2014-06-16 11:00:00")
+partido_28 = Game.where(fecha: fecha_partido_28, local: alemania, visita: portugal).first
 if not partido_28
-partido_28 = Game.new fecha: fecha_partido_28, local:argentina, visita:bosnia, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
+partido_28 = Game.new fecha: fecha_partido_28, local:alemania, visita:portugal, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
 end
 
-fecha_partido_29 = DateTime.parse("2014-06-16 11:00:00")
-partido_29 = Game.where(fecha: fecha_partido_29, local: alemania, visita: portugal).first
-if not partido_1
-partido_29 = Game.new fecha: fecha_partido_29, local:alemania, visita:portugal, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
+fecha_partido_29 = DateTime.parse("2014-06-16 14:00:00")
+partido_29 = Game.where(fecha: fecha_partido_29, local: iran, visita: nigeria).first
+if not partido_29
+partido_29 = Game.new fecha: fecha_partido_29, local:iran, visita:nigeria, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
 end
 
-fecha_partido_30 = DateTime.parse("2014-06-16 14:00:00")
-partido_30 = Game.where(fecha: fecha_partido_30, local: iran, visita: nigeria).first
+fecha_partido_30 = DateTime.parse("2014-06-16 17:00:00")
+partido_30 = Game.where(fecha: fecha_partido_30, local: ghana, visita: estados_unidos).first
 if not partido_30
-partido_30 = Game.new fecha: fecha_partido_30, local:iran, visita:nigeria, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
+partido_30 = Game.new fecha: fecha_partido_30, local:ghana, visita:estados_unidos, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
 end
 
-fecha_partido_31 = DateTime.parse("2014-06-16 17:00:00")
-partido_31 = Game.where(fecha: fecha_partido_31, local: ghana, visita: estados_unidos).first
+fecha_partido_31 = DateTime.parse("2014-06-17 11:00:00")
+partido_31 = Game.where(fecha: fecha_partido_31, local: belgica, visita: argelia).first
 if not partido_31
-partido_31 = Game.new fecha: fecha_partido_31, local:ghana, visita:estados_unidos, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
+partido_31 = Game.new fecha: fecha_partido_31, local:belgica, visita:argelia, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
 end
 
-fecha_partido_32 = DateTime.parse("2014-06-17 11:00:00")
-partido_32 = Game.where(fecha: fecha_partido_32, local: belgica, visita: argelia).first
+fecha_partido_32 = DateTime.parse("2014-06-17 17:00:00")
+partido_32 = Game.where(fecha: fecha_partido_32, local: rusia, visita: republica_de_corea).first
 if not partido_32
-partido_32 = Game.new fecha: fecha_partido_32, local:belgica, visita:argelia, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
+partido_32 = Game.new fecha: fecha_partido_32, local:rusia, visita:republica_de_corea, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
 end
 
-fecha_partido_33 = DateTime.parse("2014-06-17 17:00:00")
-partido_33 = Game.where(fecha: fecha_partido_33, local: rusia, visita: republica_de_corea).first
+fecha_partido_33 = DateTime.parse("2014-06-20 14:00:00")
+partido_33 = Game.where(fecha: fecha_partido_33, local: suiza, visita: francia).first
 if not partido_33
-partido_33 = Game.new fecha: fecha_partido_33, local:rusia, visita:republica_de_corea, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
+partido_33 = Game.new fecha: fecha_partido_33, local:suiza, visita:francia, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
 end
 
-fecha_partido_34 = DateTime.parse("2014-06-20 14:00:00")
-partido_34 = Game.where(fecha: fecha_partido_34, local: suiza, visita: francia).first
+fecha_partido_34 = DateTime.parse("2014-06-20 17:00:00")
+partido_34 = Game.where(fecha: fecha_partido_34, local: honduras, visita: ecuador).first
 if not partido_34
-partido_34 = Game.new fecha: fecha_partido_34, local:suiza, visita:francia, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
+partido_34 = Game.new fecha: fecha_partido_34, local:honduras, visita:ecuador, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
 end
 
-fecha_partido_35 = DateTime.parse("2014-06-20 17:00:00")
-partido_35 = Game.where(fecha: fecha_partido_35, local: honduras, visita: ecuador).first
+fecha_partido_35 = DateTime.parse("2014-06-21 11:00:00")
+partido_35 = Game.where(fecha: fecha_partido_35, local: argentina, visita: iran).first
 if not partido_35
-partido_35 = Game.new fecha: fecha_partido_35, local:honduras, visita:ecuador, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
+partido_35 = Game.new fecha: fecha_partido_35, local:argentina, visita:iran, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
 end
 
-fecha_partido_36 = DateTime.parse("2014-06-21 11:00:00")
-partido_36 = Game.where(fecha: fecha_partido_36, local: argentina, visita: iran).first
+fecha_partido_36 = DateTime.parse("2014-06-21 14:00:00")
+partido_36 = Game.where(fecha: fecha_partido_36, local: alemania, visita: ghana).first
 if not partido_36
-partido_36 = Game.new fecha: fecha_partido_36, local:argentina, visita:iran, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
+partido_36 = Game.new fecha: fecha_partido_36, local:alemania, visita:ghana, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
 end
 
-fecha_partido_37 = DateTime.parse("2014-06-21 14:00:00")
-partido_37 = Game.where(fecha: fecha_partido_1, local: alemania, visita: ghana).first
+fecha_partido_37 = DateTime.parse("2014-06-21 17:00:00")
+partido_37 = Game.where(fecha: fecha_partido_37, local: nigeria, visita: bosnia).first
 if not partido_37
-partido_37 = Game.new fecha: fecha_partido_1, local:alemania, visita:ghana, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
+partido_37 = Game.new fecha: fecha_partido_37, local:nigeria, visita:bosnia, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
 end
 
-fecha_partido_38 = DateTime.parse("2014-06-21 17:00:00")
-partido_38 = Game.where(fecha: fecha_partido_38, local: nigeria, visita: bosnia).first
+fecha_partido_38 = DateTime.parse("2014-06-22 11:00:00")
+partido_38 = Game.where(fecha: fecha_partido_38, local: belgica, visita: rusia).first
 if not partido_38
-partido_38 = Game.new fecha: fecha_partido_38, local:nigeria, visita:bosnia, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
+partido_38 = Game.new fecha: fecha_partido_38, local:belgica, visita:rusia, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
 end
 
-fecha_partido_39 = DateTime.parse("2014-06-22 11:00:00")
-partido_39 = Game.where(fecha: fecha_partido_39, local: belgica, visita: rusia).first
+
+fecha_partido_39 = DateTime.parse("2014-06-22 14:00:00")
+partido_39 = Game.where(fecha: fecha_partido_39, local: republica_de_corea, visita: argelia).first
 if not partido_39
-partido_39 = Game.new fecha: fecha_partido_39, local:belgica, visita:rusia, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
+partido_39 = Game.new fecha: fecha_partido_39, local:republica_de_corea, visita:argelia, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
 end
 
-
-fecha_partido_40 = DateTime.parse("2014-06-22 14:00:00")
-partido_40 = Game.where(fecha: fecha_partido_40, local: republica_de_corea, visita: argelia).first
+fecha_partido_40 = DateTime.parse("2014-06-22 17:00:00")
+partido_40 = Game.where(fecha: fecha_partido_40, local: estados_unidos, visita: portugal).first
 if not partido_40
-partido_40 = Game.new fecha: fecha_partido_40, local:republica_de_corea, visita:argelia, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
+partido_40 = Game.new fecha: fecha_partido_40, local:estados_unidos, visita:portugal, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
 end
 
-fecha_partido_41 = DateTime.parse("2014-06-22 17:00:00")
-partido_41 = Game.where(fecha: fecha_partido_41, local: estados_unidos, visita: portugal).first
+fecha_partido_41 = DateTime.parse("2014-06-25 11:00:00")
+partido_41 = Game.where(fecha: fecha_partido_41, local: nigeria, visita: argentina).first
 if not partido_41
-partido_41 = Game.new fecha: fecha_partido_41, local:estados_unidos, visita:portugal, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
+partido_41 = Game.new fecha: fecha_partido_41, local:nigeria, visita:argentina, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
 end
 
 fecha_partido_42 = DateTime.parse("2014-06-25 11:00:00")
-partido_42 = Game.where(fecha: fecha_partido_42, local: nigeria, visita: argentina).first
+partido_42 = Game.where(fecha: fecha_partido_42, local: bosnia, visita: iran).first
 if not partido_42
-partido_42 = Game.new fecha: fecha_partido_42, local:nigeria, visita:argentina, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
+partido_42 = Game.new fecha: fecha_partido_42, local:bosnia, visita:iran, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
 end
 
-fecha_partido_43 = DateTime.parse("2014-06-25 11:00:00")
-partido_43 = Game.where(fecha: fecha_partido_43, local: bosnia, visita: iran).first
+fecha_partido_43 = DateTime.parse("2014-06-25 15:00:00")
+partido_43 = Game.where(fecha: fecha_partido_43, local: honduras, visita: suiza).first
 if not partido_43
-partido_43 = Game.new fecha: fecha_partido_43, local:bosnia, visita:iran, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
+partido_43 = Game.new fecha: fecha_partido_43, local:honduras, visita:suiza, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
 end
 
-fecha_partido_44 = DateTime.parse("2014-06-25 15:00:00")
-partido_44 = Game.where(fecha: fecha_partido_44, local: honduras, visita: suiza).first
+fecha_partido_44 = DateTime.parse("2014-06-25 17:00:00")
+partido_44 = Game.where(fecha: fecha_partido_44, local: ecuador, visita: francias).first
 if not partido_44
-partido_44 = Game.new fecha: fecha_partido_44, local:honduras, visita:suiza, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
+partido_44 = Game.new fecha: fecha_partido_44, local:ecuador, visita:francia, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
 end
 
-fecha_partido_45 = DateTime.parse("2014-06-25 17:00:00")
-partido_45 = Game.where(fecha: fecha_partido_45, local: ecuador, visita: francias).first
+fecha_partido_45 = DateTime.parse("2014-06-26 11:00:00")
+partido_45 = Game.where(fecha: fecha_partido_45, local: portugal, visita: ghana).first
 if not partido_45
-partido_45 = Game.new fecha: fecha_partido_45, local:ecuador, visita:francia, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
+partido_45 = Game.new fecha: fecha_partido_45, local:portugal, visita:ghana, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
 end
 
 fecha_partido_46 = DateTime.parse("2014-06-26 11:00:00")
-partido_46 = Game.where(fecha: fecha_partido_46, local: portugal, visita: ghana).first
+partido_46 = Game.where(fecha: fecha_partido_46, local: estados_unidos, visita: alemania).first
 if not partido_46
-partido_46 = Game.new fecha: fecha_partido_46, local:portugal, visita:ghana, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
+partido_46 = Game.new fecha: fecha_partido_46, local:estados_unidos, visita:alemania, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
 end
 
-fecha_partido_47 = DateTime.parse("2014-06-26 11:00:00")
-partido_47 = Game.where(fecha: fecha_partido_47, local: estados_unidos, visita: alemania).first
+fecha_partido_47 = DateTime.parse("2014-06-26 15:00:00")
+partido_47 = Game.where(fecha: fecha_partido_47, local: republica_de_corea, visita: belgica).first
 if not partido_47
-partido_47 = Game.new fecha: fecha_partido_47, local:estados_unidos, visita:alemania, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
+partido_47 = Game.new fecha: fecha_partido_47, local:republica_de_corea, visita:belgica, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
 end
 
 fecha_partido_48 = DateTime.parse("2014-06-26 15:00:00")
-partido_48 = Game.where(fecha: fecha_partido_48, local: republica_de_corea, visita: belgica).first
+partido_48 = Game.where(fecha: fecha_partido_48, local: argelia, visita: rusia).first
 if not partido_48
-partido_48 = Game.new fecha: fecha_partido_48, local:republica_de_corea, visita:belgica, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
-end
-
-fecha_partido_49 = DateTime.parse("2014-06-26 15:00:00")
-partido_49 = Game.where(fecha: fecha_partido_49, local: argelia, visita: rusia).first
-if not partido_49
-partido_49 = Game.new fecha: fecha_partido_49, local:argelia, visita:rusia, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
+partido_48 = Game.new fecha: fecha_partido_48, local:argelia, visita:rusia, goles_visita: 0, goles_local: 0, jugado: false, resultado: 0
 end
 
 #creacion de fase
@@ -328,4 +328,27 @@ end
 partido_1.stage = fase_1
 fase_1.save
 partido_1.save
-
+partido_25.save
+partido_26.save
+partido_27.save
+partido_28.save
+partido_29.save
+partido_30.save
+partido_31.save
+partido_32.save
+partido_33.save
+partido_34.save
+partido_35.save
+partido_36.save
+partido_37.save
+partido_38.save
+partido_39.save
+partido_40.save
+partido_41.save
+partido_42.save
+partido_43.save
+partido_44.save
+partido_45.save
+partido_46.save
+partido_47.save
+partido_48.save
