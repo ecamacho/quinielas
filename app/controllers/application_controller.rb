8c4cab5dee59a995b7da7fbed0314e4815bc0f65
gustavo <pachequin44@hotmail.com>
2014-06-30 13:06:21 -0500

    error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,12 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  
+  def denegar_acceso
+  	redirect_to url_for(:controller => :login, :action => :index)
+  end
   helper_method :current_user
+  helper_method :denegar_acceso
 end
 
